43e3f485a20bfbc3b1338896ae6baf4f2f8f23ae
Theo Diamantidis <diamaltho@gmail.com>
2018-12-01 13:54:43 +0200

    Created API application

---
+++ project/api/models/__init__.py
@@ -0,0 +1,11 @@
+'''
+Import all models here.
+
+Instead of having a models.py file, we can define a module
+with separate files which are then imported, as done here.
+
+Django uses "project.api.models", so this will work anyway.
+
+Example usage:
+    from .[model_name] import *
+'''
