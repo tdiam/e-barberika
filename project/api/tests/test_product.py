e16ec31ce2292949fe4c66ccf81447e0faa590fd
Nick Loukas <nickloukas@outlook.com>
2018-12-05 23:47:56 +0200

    Finished test_product.py and now i'm waiting for villaras123

--- project/api/tests/test_product.py
+++ project/api/tests/test_product.py
@@ -2,3 +2,21 @@ from django.test import TestCase
 from ..models import Product
 
 
+class ProductTestCase(TestCase):
+    ''' This is the test suite for the Product Model '''
+
+    def setUp(self):
+        self.product = Product(
+            product_name ='Κούρεμα Playmobil',
+            store_name = 'Εξαδάκτυλος',
+            price = 3.0
+        )
+
+    def test_can_create_product(self):
+        num_of_products_before = Product.objects.count()
+
+        self.product.save()
+
+        num_of_products_after = Product.objects.count()
+
+        self.assertEqual(num_of_products_before + 1, num_of_products_after)
