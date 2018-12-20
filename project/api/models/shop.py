0e3621977bd9d84e7f99d5e986249ec9e058e983
Theo Diamantidis <diamaltho@gmail.com>
2018-12-20 17:14:50 +0200

    Added GeoDjango and added fields in Shop model

--- project/api/models/shop.py
+++ project/api/models/shop.py
@@ -1,5 +1,31 @@
-from django.db import models
+from django.contrib.gis.db import models
+
+
+class ShopTags(models.Model):
+    tag = models.CharField(primary_key=True, max_length=255)
+
+
+class ShopManager(models.Manager):
+    def get_queryset(self):
+        '''Omit withdrawn shops from results by default'''
+        return super().get_queryset().filter(withdrawn=False)
+
 
 class Shop(models.Model):
-    '''[Enter description for the model here]'''
+    '''Physical places where the products of our application are sold'''
+    
+    # Long integers are required from the specification
+    id = models.BigAutoField(primary_key=True)
+
     name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
+    # We will use GeoDjango for storing and querying spatial data
+    # https://docs.djangoproject.com/en/2.1/ref/contrib/gis/
+    coordinates = models.PointField()
+
+    withdrawn = models.BooleanField(default=False)
+
+    tags = models.ManyToManyField(ShopTags)
+
+    objects = ShopManager()
--- project/settings/base.py
+++ project/settings/base.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'project.token_auth',
     'project.api',
 ]
