51cc23ab12095ed5cda568f188ebcaef6e92b156
Theodore Diamantidis <diamaltho@gmail.com>
2018-12-22 16:05:36 +0200

    Shop model (#1)
    * Created test suite for Shop
    
    * Created minimal Shop model (only name field)
    
    * Added GeoDjango and added fields in Shop model
    
    * Enabled geography mode in shop coordinates; Added custom within_distance_from manager method
    
    * Wrote tests for Shop models; ShopTags -> ShopTag

--- project/api/admin.py
+++ project/api/admin.py
@@ -1,3 +1,7 @@
-# from django.contrib import admin
+from django.contrib import admin
 
 # Register your models here.
+from .models import Shop
+
+
+admin.site.register(Shop)
---
+++ project/api/models/shop.py
@@ -0,0 +1,62 @@
+from django.contrib.gis.db import models
+from django.contrib.gis.geos import Point
+from django.contrib.gis.measure import Distance
+
+
+class ShopTag(models.Model):
+    tag = models.CharField(primary_key=True, max_length=255)
+
+    def __str__(self):
+        return self.tag
+
+
+class ShopManager(models.Manager):
+    def get_queryset(self):
+        '''Omit withdrawn shops from results by default'''
+        return super().get_queryset().filter(withdrawn=False)
+
+    def within_distance_from(self, lat, lng, **distance):
+        '''Find shops within given distance from given point.
+
+        Arguments:
+            - lat: Latitude of reference point.
+            - lng: Longitude of reference point.
+            - **distance: `Distance` arguments. For supported units see:
+                https://docs.djangoproject.com/en/2.1/ref/contrib/gis/measure/#supported-units
+
+        Example:
+            ```
+            shops = Shop.objects.within_distance_from(39.89, 22.18, km=10)
+            ```
+        '''
+        return self.get_queryset().filter(
+            coordinates__dwithin=(Point(lng, lat), Distance(**distance))
+        )
+
+
+class Shop(models.Model):
+    '''Physical places where the products of our application are sold'''
+
+    # Long integers are required from the specification
+    id = models.BigAutoField(primary_key=True)
+
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
+    # We will use GeoDjango for storing and querying spatial data
+    # https://docs.djangoproject.com/en/2.1/ref/contrib/gis/
+    # geography=True will enable distance lookups that use metric
+    # https://docs.djangoproject.com/en/2.1/ref/contrib/gis/model-api/#geography
+    #
+    # NOTE: If the field is set via Python and the Point model is used,
+    # the syntax is Point(longitude, latitude).
+    coordinates = models.PointField(geography=True)
+
+    withdrawn = models.BooleanField(default=False)
+
+    tags = models.ManyToManyField(ShopTag)
+
+    objects = ShopManager()
+
+    def __str__(self):
+        return self.name
