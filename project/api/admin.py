aeeae13e9b242ef3b21ccb9c108d1994aaa353bd
Theo Diamantidis <diamaltho@gmail.com>
2018-12-22 16:08:24 +0200

    [FIX] Forgot ShopTag registration in admin.py

--- project/api/admin.py
+++ project/api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Shop
+from .models import Shop, ShopTag
 
 
 admin.site.register(Shop)
+admin.site.register(ShopTag)
