1a904eefc39249c9c2a7010a8ca14ff030d58c2c
Aggelos Kolaitis <neoaggelos@gmail.com>
2018-12-26 23:34:54 +0200

    added 'logout/' URL

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from project.token_auth.views import LoginView
+from project.token_auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='api-login'),
+    path('logout/', LogoutView.as_view(), name='api-logout')
 ]
