43ea38272dabffc544d959915af7060952ef2850
Aggelos Kolaitis <neoaggelos@gmail.com>
2019-01-11 03:08:11 +0200

    added API/register/ url

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from project.token_auth.views import LoginView, LogoutView
+from project.token_auth.views import LoginView, LogoutView, RegisterView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='api-login'),
-    path('logout/', LogoutView.as_view(), name='api-logout')
+    path('logout/', LogoutView.as_view(), name='api-logout'),
+    path('register/', RegisterView.as_view(), name='api-register')
 ]
