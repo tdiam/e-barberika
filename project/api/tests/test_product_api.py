4431b04135aef02825f2d205bac3afdbacc28b0e
Nick Loukas <nickloukas@outlook.com>
2019-01-14 02:13:41 +0200

    Finished code for ProductView needs debug

--- project/api/tests/test_product_api.py
+++ project/api/tests/test_product_api.py
@@ -24,9 +24,6 @@ class ProductsGetTestCase(TestCase):
         for _ in range(50):
             products.append(Product(
                 name=fake.first_name(),
-                address=fake.address(),
-                description=fake.description(),
-                category=fake.category()
             ))
 
         Product.objects.bulk_create(products)
@@ -40,3 +37,128 @@ class ProductsGetTestCase(TestCase):
         self.assertEqual(data['start'], 0)
         self.assertEqual(data['count'], 20)
         self.assertEqual(data['total'], 50)
+
+    def test_single_product_is_in_response(self):
+        '''Check if the default response contains the first product's name'''
+        # Default order
+        product = Product.objects.order_by('-id')[0]
+
+        req = self.factory.get(self.url)
+        res = self.view(req)
+
+        self.assertContains(res, product.name)
+
+    def test_pagination_works(self):
+        '''Check if setting the count returns the correct number of products'''
+        req = self.factory.get(self.url, {'count': 30})
+        res = self.view(req)
+
+        data = json.loads(res.content)
+        self.assertEqual(len(data['products']), 30)
+
+    def test_count_greater_than_total(self):
+        '''Count greater than total number of products should not affect response'''
+        req = self.factory.get(self.url, {'count': 999999999999})
+        res = self.view(req)
+
+        data = json.loads(res.content)
+        self.assertEqual(len(data['products']), 50)
+
+    def test_bad_parameters(self):
+        '''Wrong format of parameters should return 400 Bad Request'''
+        bad_param_sets = [
+            {'start': -10},
+            {'start': 'hello'},
+            {'start': 0, 'count': -1},
+            {'status': 'YOLO'},
+            {'status': 51},
+            {'sort': 'id|DESK'},
+            {'sort': 'name'},
+            {'sort': 'id|DESC|ASC|DESC'},
+        ]
+
+        for i, bad_params in enumerate(bad_param_sets):
+            with self.subTest(i=i):
+                req = self.factory.get(self.url, bad_params)
+                res = self.view(req)
+                self.assertEqual(res.status_code, 400)
+
+    def test_status_parameter(self):
+        '''Check total number of products returned for each possible value of `status`'''
+        # Get 10 products
+        ten_products = Product.objects.values_list('id')[:10]
+        # Withdraw them
+        Product.objects.filter(id__in=ten_products).update(withdrawn=True)
+
+        # When status is ACTIVE (default)
+        with self.subTest(msg='ACTIVE status'):
+            req = self.factory.get(self.url)
+            res = self.view(req)
+            data = json.loads(res.content)
+            self.assertEqual(data['total'], 40)
+
+        # When status is ALL
+        with self.subTest(msg='ALL status'):
+            req = self.factory.get(self.url, {'status': 'ALL'})
+            res = self.view(req)
+            data = json.loads(res.content)
+            self.assertEqual(data['total'], 50)
+
+        # When status is WITHDRAWN
+        with self.subTest(msg='WITHDRAWN status'):
+            req = self.factory.get(self.url, {'status': 'WITHDRAWN'})
+            res = self.view(req)
+            data = json.loads(res.content)
+            self.assertEqual(data['total'], 10)
+
+
+class ProductsPostTestCase(TestCase):
+    '''Unit test for POST /products'''
+    def setUp(self):
+        # Get URL for /products
+        self.url = reverse('products')
+        self.factory = RequestFactory()
+        self.view = ProductsView.as_view()
+
+        fake = Faker('el_GR')
+        self.data = dict(
+            name=fake.first_name(),
+        )
+
+    def test_create_with_good_data(self):
+        '''Check with normal data to see if 201 is returned and the entry is created'''
+        req = self.factory.post(self.url, self.data)
+        res = self.view(req)
+
+        self.assertEqual(res.status_code, 201)
+        self.assertTrue(Product.objects.filter(name=self.data['name']).exists())
+
+    def test_create_with_bad_data(self):
+        '''Try omitting or setting bad values for each field to see if 400 Bad Request is returned'''
+        # Helper functions
+        def _set(key, val):
+            '''Copies self.data and sets d[key] to val'''
+            res = copy.deepcopy(self.data)
+            res[key] = val
+            return res
+
+        def _del(key):
+            '''Copies self.data and removes d[key]'''
+            res = copy.deepcopy(self.data)
+            res.pop(key, None)
+            return res
+
+        datasets = [
+            _del('name'),
+            _set('name', ''),
+            _del('description'),
+            _set('description', ''),
+            _del('category'),
+            _set('category', ''),
+        ]
+
+        for i, data in enumerate(datasets):
+            with self.subTest(i=i):
+                req = self.factory.post(self.url, data)
+                res = self.view(req)
+                self.assertEqual(res.status_code, 400)
--- project/api/urls.py
+++ project/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from project.token_auth.views import ObtainTokenLoginView
+from .views import ProductsView
 
 urlpatterns = [
     path('login/', ObtainTokenLoginView.as_view(), name='api-login'),
+    path('products/', ProductsView.as_view(), name='products'),
 ]
