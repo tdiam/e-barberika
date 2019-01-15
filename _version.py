97c5d4fc53f43f321ffbb6cc0adbb6ca1789e382
Theo Diamantidis <diamaltho@gmail.com>
2019-01-15 02:09:41 +0200

    Django 2.1.2 -> 2.1.5 due to security alert; Bumped version

--- _version.py
+++ _version.py
@@ -1,3 +1,3 @@
 '''Calendar versioning (https://calver.org/) is used, since it is more suitable for websites than semver.'''
 
-__version__ = '2018.12.20-prealpha'
+__version__ = '2019.01.14-prealpha'
