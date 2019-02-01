a6dcd9a5415700ed3e0b9816f8b8c8bdfe239f85
Theo Diamantidis <diamaltho@gmail.com>
2019-02-01 15:29:48 +0100

    Added axios library and custom interceptor to send urlencoded-formatted requests

--- project/client/src/utils/api.js
+++ project/client/src/utils/api.js
@@ -1,5 +1,23 @@
 import axios from 'axios'
+import qs from 'qs'
 
-export default () => axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+const instance = axios.create({
+    baseURL: process.env.REACT_APP_API_URL,
+    headers: {
+        common: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    }
 })
+
+const qsConfig = {
+    indices: false
+}
+
+export default {
+    get: (url, params) => instance.get(url, { params }),
+    post: (url, data) => instance.post(url, qs.stringify(data, qsConfig)),
+    put: (url, data) => instance.put(url, qs.stringify(data, qsConfig)),
+    patch: (url, data) => instance.patch(url, qs.stringify(data, qsConfig)),
+    delete: (url) => instance.delete(url),
+}
