9a6e7fb4c3b989481179665cbcf8d732ca7f05d2
Yiorgos Chochlakis <thisisgc10@gmail.com>
2019-02-17 22:32:47 +0200

    Started working on Home Page search-bar, created relative components

---
+++ project/client/src/components/SearchFilters.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SearchFilters extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
--- project/client/src/pages/Home.js
+++ project/client/src/pages/Home.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react'
+import SearchBar from '../components/SearchBar';
+// import SearchField from 'react-search-field';
+import SearchResults from '../components/SearchResults';
+import SearchFilters from '../components/SearchFilters';
+
 
 class Home extends Component {
+    
+    state = {
+        query: '',
+        filter: {}
+    }
+
+    setQuery = (query) => {
+        console.log(query)
+        this.setState({ query })
+    }
+
+    setFilters = (filters) => {
+        this.setState({ filters })
+    }
+
+    showFilters = () => {
+        return this.state.query !== ''
+    }
+
     render() {
         return (
-            <div>Welcome to Asoures!</div>
+            <React.Fragment>
+                <div>Welcome to Asoures!</div>
+                <SearchBar setQuery={ this.setQuery } />
+                <SearchFilters
+                    display={ /* this.showFilters() */ false }
+                    setFilters={ this.setFilters } 
+                />
+                <SearchResults  
+                    query={ this.state.query }
+                    filters={ this.state.filters }
+                />
+            </React.Fragment>
         )
     }
 }
