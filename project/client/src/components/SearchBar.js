4764a0a9fff14d60c3aeb7e7fc62b7c45f97a419
Yiorgos Chochlakis <thisisgc10@gmail.com>
2019-02-17 22:32:47 +0200

    Started working on Home Page search-bar, created relative components

---
+++ project/client/src/components/SearchBar.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+
+export default class SearchBar extends Component {  
+  
+  state = {
+      searchbar: '',
+  }
+
+  handleChange = (e) => {
+    this.setState({
+        [e.target.name]: e.target.value
+    })
+    console.log(this.state.searchbar)
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault()
+    this.props.setQuery(this.state.searchbar)
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={ this.submitHandler }>  
+            <input 
+                type="text" 
+                name="searchbar" 
+                id="searchbar"
+                onKeyPress={ this.handleChange }
+                text={ this.state.searchbar }
+            />
+            <button type="submit">-></button>
+        </form>
+      </div>
+    )
+  }
+}
+
+SearchBar.propTypes = {
+    setQuery: PropTypes.func.isRequired,
+}
---
+++ project/client/src/components/SearchResults.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SearchResults extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
