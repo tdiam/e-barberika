164b1209e87bfd57eae6824681f65397b30f81a0
Aggelos Kolaitis <neoaggelos@gmail.com>
2019-02-25 00:43:27 +0200

    added 404 page

--- project/client/src/partials/Main.js
+++ project/client/src/partials/Main.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import Home from '../pages/Home'
 import LoginForm from '../pages/LoginForm'
 import ShopListing from '../pages/ShopListing'
 import AddShop from '../pages/AddShop'
 import MapDemo from '../pages/MapDemo'
-import SignupForm from '../pages/SignupForm';
+import SignupForm from '../pages/SignupForm'
+import Page404 from '../pages/Page404'
 
 class Main extends Component {
     render() {
         return (
-            <>
+            <Switch>
                 <Route exact path="/" component={ Home } />
                 <Route exact path="/login" component={ LoginForm } />
                 <Route exact path="/shops" component={ ShopListing } />
                 <Route exact path="/shops/add" component={ AddShop } />
                 <Route exact path="/map" component={ MapDemo } />
                 <Route exact path="/signup" component={ SignupForm } />
-            </>
+                <Route component={ Page404 } />
+            </Switch>
         )
     }
 }
