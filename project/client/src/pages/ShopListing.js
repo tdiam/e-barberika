f367dd96addfe18e260a019d2938b20cd1cd7309
Aggelos Kolaitis <neoaggelos@gmail.com>
2019-02-28 01:31:05 +0200

    [frontend] Began working on shops list

--- project/client/src/pages/ShopListing.js
+++ project/client/src/pages/ShopListing.js
@@ -1,25 +1,77 @@
 import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react'
+import MaterialTable from 'material-table'
 
 class ShopListing extends Component {
     constructor(props) {
         super(props)
+        this.root = this.props.store
         this.store = this.props.store.shopStore
+
+        this.columns = [{
+            title: 'Όνομα καταστήματος',
+            field: 'name',
+        }, {
+            title: 'Διεύθυνση',
+            field: 'address',
+        }, {
+            title: 'Κρυμμένο',
+            field: 'withdrawn',
+            type: 'boolean'
+        }]
+
+        this.actions = [{
+            icon: 'delete',
+            orig: this,
+            tooltip: 'Διαγραφή Καταστήματος',
+            onClick: this.called
+        }]
     }
-    componentDidMount() {
+
+    async loadShops() {
         // Execute API call that will update the store state
-        this.store.getShops()
+        // NOTE: not too good if we have too many shops
+        await this.store.getShops({count:0})
+        await this.store.getShops({count:this.store.pagination.total, status: 'ALL'})
     }
+
+    async componentDidMount() {
+        await this.loadShops()
+    }
+
+    async called(e, row) {
+        console.log(this)
+        console.log("Clicked on", row.name)
+
+        await this.orig.store.deleteShop(row.id)
+        await this.orig.loadShops()
+    }
+
     render() {
-        let shopItems = this.store.shops.map(shop => (
-            <li key={ shop.id }>{ shop.name }</li>
-        ))
+        if (! this.root.isLoggedIn) {
+            return (
+                <div>
+                    You must log in
+                </div>
+            )
+        }
+        if (this.store.state === 'done') {
+            return (
+                <MaterialTable 
+                    data={this.store.shops}
+                    columns={this.columns}
+                    title={"Shops"}
+                    actions={this.actions}
+                    options={{
+                        actionsColumnIndex: -1,
+                        pageSize: 10
+                    }}
+                />
+            )
+        }
         return (
             <div>
-                <h2>Shops:</h2>
-                <ul>
-                    { shopItems }
-                </ul>
+                <p>pending</p>
             </div>
         )
     }
