0da50f3aac064857d89466b781c6d67c8a7bf2de
Theo Diamantidis <diamaltho@gmail.com>
2019-02-28 10:05:25 +0100

    Fixed getCurrentDate helper

--- project/client/src/utils/getCurrentDate.js
+++ project/client/src/utils/getCurrentDate.js
@@ -3,6 +3,9 @@ export function getCurrentDate (separator = '-') {
   let date = newDate.getDate()
   let month = newDate.getMonth() + 1
   let year = newDate.getFullYear()
+  if (month < 10) {
+    month = `0${month}`
+  }
 
-  return `${year}${separator}${month < 10 ? `0${month}` : `${month}`}${separator}${date}`
+  return `${year}${separator}${month}${separator}${date}`
 }
