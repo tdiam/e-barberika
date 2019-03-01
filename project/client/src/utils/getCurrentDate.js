ae7f8a1359e0774effd5fea49d93f28b5b4ed2b1
Yiorgos Chochlakis <yiorgoschochlakis@gmail.com>
2019-03-01 14:50:35 +0200

    Day format fix

--- project/client/src/utils/getCurrentDate.js
+++ project/client/src/utils/getCurrentDate.js
@@ -1,6 +1,9 @@
 export function getCurrentDate (separator = '-') {
   let newDate = new Date()
   let date = newDate.getDate()
+  if (date < 10) {
+    date = `0${date}`
+  }
   let month = newDate.getMonth() + 1
   let year = newDate.getFullYear()
   if (month < 10) {
