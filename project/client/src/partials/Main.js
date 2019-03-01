9e5b7b3728f539a0e899ac6d3f1df98b75927d52
Theo Diamantidis <diamaltho@gmail.com>
2019-03-01 18:46:13 +0100

    Wrapped main in container-fluid

--- project/client/src/partials/Main.js
+++ project/client/src/partials/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
+import { Container } from 'reactstrap'
 
 import Home from '../pages/Home'
 import LoginForm from '../pages/LoginForm'
@@ -14,17 +15,19 @@ import Page404 from '../pages/Page404'
 class Main extends Component {
   render () {
     return (
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/login' component={LoginForm} />
-        <Route exact path='/shops' component={ShopListing} />
-        <Route path='/shops/:id' component={Shop} />
-        <Route exact path='/products' component={ProductListing} />
-        <Route path='/products/:id' component={Product} />
-        <Route exact path='/map' component={MapDemo} />
-        <Route exact path='/signup' component={SignupForm} />
-        <Route component={ Page404 } />
-      </Switch>
+      <Container fluid tag="main">
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={LoginForm} />
+          <Route exact path='/shops' component={ShopListing} />
+          <Route path='/shops/:id' component={Shop} />
+          <Route exact path='/products' component={ProductListing} />
+          <Route path='/products/:id' component={Product} />
+          <Route exact path='/map' component={MapDemo} />
+          <Route exact path='/signup' component={SignupForm} />
+          <Route component={ Page404 } />
+        </Switch>
+      </Container>
     )
   }
 }
